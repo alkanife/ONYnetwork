0c88fab3f6034f7d9f1b5d0340069b560fc8dd9e
Cardon Alban <73275347+albancardon@users.noreply.github.com>
2023-06-14 16:56:54 +0200

    save jour 01
    commencement javascript

--- ONYnetwork-website/wwwroot/js/site.js
+++ ONYnetwork-website/wwwroot/js/site.js
@@ -1,4 +1,40 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+//1. changement de monnaie
+// récupération des liens pour changer
 
-// Write your JavaScript code.
+const moneyUse = document.getElementById("moneyUse");
+const currencyEuro = document.getElementById("changeCurrencyEuro");
+const currencyDollar = document.getElementById("changeCurrencyDollar");
+
+
+//ajout des events listener
+
+window.onload = moneyEuro();
+currencyEuro.addEventListener("click", moneyEuro);
+currencyDollar.addEventListener("click", moneyDol);
+
+
+// fonction pour modifer la monnaie
+
+function moneyEuro() {
+    moneyUse.innerHTML = "la monnaie utiliser est l'Euro";
+}
+function moneyDol() {
+    moneyUse.innerHTML = "la monnaie utiliser est le Dollar";
+}
+
+//2. offre promo ouverture de la modale au chargement de la page
+// récupération des liens pour ouvrir la modale
+
+const modalPromo = document.getElementById("modalPromo");
+
+//ajout des events listener
+
+window.onload = modalPromo;
+function ouvertureModale() {
+    modalPromo.modal('show')
+}
+var myModal = new bootstrap.Modal(modalPromo, {});
+document.onreadystatechange = function () {
+    myModal.show();
+};
